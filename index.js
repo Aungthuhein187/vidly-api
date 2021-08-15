cc039be21d16d2e001c7c405e397d4fd2c1992f1
Aung Thu Hein <aungthuhein187@gmail.com>
2021-08-15 18:01:25 +0630

    Add joi for validation request body and genres route.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
-const home = require("./routes/home");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const app = express();
+
+const genres = require("./routes/genres");
+const home = require("./routes/home");
 
 app.use(express.json());
 app.use(helmet());
 if (app.get("env") === "development") app.use(morgan("dev"));
 
 app.use("/", home);
+app.use("/api/genres", genres);
 
 const port = process.env.NODE_ENV || 3000;
 app.listen(port, () => {
