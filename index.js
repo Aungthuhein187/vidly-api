fa55ec95695445a049bc21871a8e63aeecfa244c
Aung Thu Hein <aungthuhein187@gmail.com>
2021-08-15 17:32:34 +0630

    Add helmet and morgan http logger.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const home = require("./routes/home");
+const helmet = require("helmet");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
+if (app.get("env") === "development") app.use(morgan("dev"));
+
 app.use("/", home);
 
 const port = process.env.NODE_ENV || 3000;
