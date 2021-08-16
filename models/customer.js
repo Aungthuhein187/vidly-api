77832b52eb79d1dce19a9f1d8e55b3367e98c7b8
aunghein <aungthuhein187@gmail.com>
2021-08-16 19:53:45 +0630

    Add customers route.

---
+++ models/customer.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const Customer = mongoose.model(
+  "Customer",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+    },
+    isGold: {
+      type: Boolean,
+      default: false,
+    },
+    phone: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+    },
+  })
+);
+
+const validateCustomer = (customer) => {
+  const customerSchema = Joi.object({
+    name: Joi.string().min(5).max(255).required().label("Name"),
+    isGold: Joi.boolean(),
+    phone: Joi.string().min(5).max(255).required().label("Phone"),
+  });
+
+  return customerSchema.validate(customer);
+};
+
+exports.Customer = Customer;
+exports.validate = validateCustomer;
