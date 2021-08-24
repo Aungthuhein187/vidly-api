3e0194a86ea84b2a8f8a1e9a2012d206f7916317
aunghein <aungthuhein187@gmail.com>
2021-08-24 15:59:45 +0630

    Test GET / route.

---
+++ tests/integration/routes/home.test.js
@@ -0,0 +1,18 @@
+const request = require("supertest");
+
+describe("/", () => {
+  let server;
+
+  beforeEach(() => {
+    server = require("../../../index");
+  });
+
+  afterEach(() => {
+    server.close();
+  });
+  it("GET /", async () => {
+    const res = await request(server).get("/").expect(200);
+
+    expect(res.text).toContain("Hello World");
+  });
+});
