28631e7b763518f3bcd7fedfdd3a5d99fe5f792c
Aung Thu Hein <aungthuhein187@gmail.com>
2021-08-25 19:58:17 +0630

    Refractor lookup & return methods from rental routes.

--- routes/returns.js
+++ routes/returns.js
@@ -1,6 +1,5 @@
 const _ = require("lodash");
 const auth = require("../middlewares/auth");
-const moment = require("moment");
 const validate = require("../middlewares/validate");
 const validateReturns = require("../validator/returns");
 const express = require("express");
@@ -9,10 +8,7 @@ const { Movie } = require("../models/movie");
 const { Rental } = require("../models/rental");
 
 router.post("/", [auth, validate(validateReturns)], async (req, res) => {
-  const rental = await Rental.findOne({
-    "customer._id": req.body.customerId,
-    "movie._id": req.body.movieId,
-  });
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
 
   if (!rental)
     return res
@@ -22,10 +18,7 @@ router.post("/", [auth, validate(validateReturns)], async (req, res) => {
   if (rental.dateReturned)
     return res.status(404).send("Genre with the given id is already returned.");
 
-  rental.dateReturned = new Date();
-
-  const diff = moment().diff(rental.dateOut, "days");
-  rental.rentalFee = rental.movie.dailyRentalRate * diff;
+  rental.return();
 
   await rental.save();
 
